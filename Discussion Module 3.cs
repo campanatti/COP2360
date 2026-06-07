0c410d1d8f842536cd9c84ae6ab7bbf2587a8786
campanatti <carolina.campanatti@my.pbsc.edu>
2026-06-07 16:20:48 -0400

    Add files via upload

---
+++ Discussion Module 3.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class Panda
+{
+    public Panda Mate;
+
+    public string Name { get; set; }   // Property
+
+    public void Marry(Panda partner)
+    {
+        Mate = partner;
+        partner.Mate = this;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create instances of Panda
+        Panda panda1 = new Panda();
+        panda1.Name = "Po";
+
+        Panda panda2 = new Panda();
+        panda2.Name = "Ling";
+
+        // Call the method
+        panda1.Marry(panda2);
+
+        // Display results
+        Console.WriteLine($"{panda1.Name} is married to {panda1.Mate.Name}");
+        Console.WriteLine($"{panda2.Name} is married to {panda2.Mate.Name}");
+    }
+}
